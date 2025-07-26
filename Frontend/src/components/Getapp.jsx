3121a234b5230354a85a62f9a21cc05a17a57883
Baljir0901 <baljir0901@gmail.com>
2025-07-27 05:32:17 +0900

    Initial commit: Bodol Threads App

---
+++ Frontend/src/components/Getapp.jsx
@@ -0,0 +1,10 @@
+const Getapp = () => {
+  return (
+    <div className='flex items-center justify-between py-4 px-2'>
+        <h1>👋 Threads App</h1>
+        <a href='https://mail.google.com/mail/u/0/#inbox?compose=jrjtXVbTxPsVJvcdCzwfthGlSlzwQQvCcLPMWnsFsHQwxPpVNVDTfjMpWJdVSGnXLNBHPHXx' className='px-4 py-2 bg-zinc-800 font-semibold text-white rounded-lg text-sm hover:bg-zinc-700'>Report a bug</a>
+    </div>
+  )
+}
+
+export default Getapp
