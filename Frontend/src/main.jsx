58c1b770e521dc73f30b4fdf66e3bf88cb270325
Baljir0901 <baljir0901@gmail.com>
2025-07-27 06:04:49 +0900

    fix: set axios baseURL to VITE_API_BASE_URL for Vercel deployment

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
